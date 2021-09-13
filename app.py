9a58314daaa1a333e23662824179534592c5ef16
Yassinejabori <74773233+Yassinejabori@users.noreply.github.com>
2021-09-13 12:57:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
-import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.externals import joblib
+import joblib
 import pickle
 
 # load the model from disk
